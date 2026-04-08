56efb00af7c991ce721aec1fdc10786475b2801a
Carlton Decker <deckercr@hotmail.com>
2026-04-08 11:43:33 -0500

    feat: scaffold Spring Boot project with all dependencies

---
+++ backend/src/main/java/com/friday/FridayApplication.java
@@ -0,0 +1,11 @@
+package com.friday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FridayApplication.class, args);
+    }
+}
